01097e91f37a497caf380956948803170b6cb251
Matthew Danyluik <danyluik@ualberta.ca>
2020-08-16 12:53:06 -0600

    Update send_art_stream.py

--- send_art_stream.py
+++ send_art_stream.py
@@ -1,10 +1,14 @@
 """Example program to demonstrate how to send a multi-channel time series to
 LSL."""
 
+import numpy as np
 import random
 import time
 
 from pylsl import StreamInfo, StreamOutlet
+
+channels = 16
+srate = 250
 
 def sendingData():
     # first create a new stream info (here we set the name to BioSemi,
@@ -12,13 +16,13 @@ def sendingData():
     # last value would be the serial number of the device or some other more or
     # less locally unique identifier for the stream as far as available (you
     # could also omit it but interrupted connections wouldn't auto-recover).
-    info = StreamInfo('BioSemi', 'EEG', 16, 250, 'float32', 'myuid34234')
+    info = StreamInfo('BioSemi', 'EEG', channels, srate, 'float32', 'myuid34234')
 
     # next make an outlet
     outlet = StreamOutlet(info)
 
     print("now sending data...")
-    
+
     alpha_low_bound = 8
     beta_low_bound = 16
     delta_low_bound = 0
@@ -41,11 +45,11 @@ def sendingData():
     baseline = np.sum(alpha,beta,delta,theta)
 
 
-    real_fake_alpha = np.sin(np.ones((250,)) * (x * (alpha_low_bound+3))) * 6 
+    real_fake_alpha = np.sin(np.ones((250,)) * (x * (alpha_low_bound+3))) * 6
     real_fake_beta = np.sin(np.ones((250,)) * (x * (beta_low_bound+8))) * 2
     real_fake_delta = np.sin(np.ones((250,)) * (x * (delta_low_bound+2))) * 18
     real_fake_theta = np.sin(np.ones((250,)) * (x * (theta_low_bound+1.5))) * 0
-    
+
     real_fake_data = np.sum(real_fake_alpha,real_fake_beta,real_fake_delta,real_fake_theta)
 
 
@@ -54,7 +58,7 @@ def sendingData():
     # beta_random = random.random() * 10
     # delta_random = random.random() * 4
     # theta_random = random.random() * 3
-    
+
     # alpha = np.sin(np.ones((250,)) * np.pi/(alpha_low_bound+alpha_random)
     # beta = np.sin(np.ones((250,)) * np.pi/(beta_low_bound+beta_random))
     # delta = np.sin(np.ones((250,)) * np.pi/(delta_low_bound+delta_random))
@@ -63,24 +67,15 @@ def sendingData():
 
     count = 0
     while True:
-        # make a new random 8-channel sample; this is converted into a
+        # make a new random 16-channel sample; this is converted into a
         # pylsl.vectorf (the data type that is expected by push_sample)
         if count < 2500:
             baseline_current = baseline[count % 250]
-            mysample = [baseline_current, baseline_current, baseline_current,
-                        baseline_current, baseline_current, baseline_current,
-                        baseline_current, baseline_current, baseline_current,
-                        baseline_current, baseline_current, baseline_current,
-                        baseline_current, baseline_current, baseline_current,
-                        baseline_current]
+            mysample = list(np.full(shape=channels, fill_value=baseline_current, dtype=np.int))
+
         else:
             real_fake_data_current = real_fake_data[count % 250]
-            mysample = [real_fake_data_current, real_fake_data_current, real_fake_data_current,
-                        real_fake_data_current, real_fake_data_current, real_fake_data_current,
-                        real_fake_data_current, real_fake_data_current, real_fake_data_current,
-                        real_fake_data_current, real_fake_data_current, real_fake_data_current,
-                        real_fake_data_current, real_fake_data_current, real_fake_data_current,
-                        real_fake_data_current]
+            mysample = list(np.full(shape=channels, fill_value=real_fake_data_current, dtype=np.int))
 
         # now send it and wait for a bit
         outlet.push_sample(mysample)
