cc5904d52bd6c54c632d7c6df023c13f707f9c5c
qhansen628 <47458953+qhansen628@users.noreply.github.com>
2020-09-04 17:02:58 -0600

    integrated live classifier communication
    running stream sends to artScreen through band_q
    
    running stream sends to classifier through model_q
    
    model connects to artDialog through art_q

--- Image_Manipulation/artScreen.py
+++ Image_Manipulation/artScreen.py
@@ -10,14 +10,14 @@ import sys
 import numpy as np
 import pdb
 from circleArt import circleArt
-
+from ..classifier import LiveModel
 class artScreen(QDialog):
 
     """ This is a window that will draw an art on the screen """
     """ inputSize is a list or a tuple of the width and the height of the screen """
     """ App is the PyQt5.QtCore.QApplication object for the main app """
 
-    def artDialog(self, inputSize, artFeatures, q):
+    def artDialog(self, inputSize, artFeatures, band_q,art_q):
         
         # initialize an array for the image -- x by y arrays of 3-item arrays for the HSV values of each pixel in the image
         self.imageArray = np.zeros((inputSize[0],inputSize[1],3),dtype=np.uint8)
@@ -27,7 +27,7 @@ class artScreen(QDialog):
         self.initUI()
         # Set timer for a set interval
         timer = QTimer(self)
-        timer.timeout.connect(lambda: self.updateScreen(q))
+        timer.timeout.connect(lambda: self.updateScreen(band_q,art_q))
         timer.start(1000)  # in milliseconds e.g. 1000 = 1 sec
 
             # set timer for creating a pulsating illusion on the image
@@ -48,13 +48,16 @@ class artScreen(QDialog):
         self.raise_()
         self.show()
 
-    def updateScreen(self,q,pulse=True):
+    def updateScreen(self,band_q,art_q,pulse=True):
 
         ''' Update the art screen
 
         funcName is the art function to be implemented. hsvArt produces a new image, and pulseArt creates a pulsing effect
         '''
-        if not q.empty():
+        if not art_q.empty():
+            emotion_array = art_q.get() #shape [negative, neutral, positive] softmax output
+            
+        if not band_q.empty():
             state_dict,noise_dict = q.get()
             print(state_dict)
             newImage = hsvArt(self.size, noise_dict, state_dict, self.artFeatures) #create newImage from hsvArt fun
@@ -75,24 +78,35 @@ class artScreen(QDialog):
                                 QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
-            run_process.terminate() #if we close the window the spawned child process terminates
+            run_process.terminate() 
             while run_process.is_alive() == True:
                 time.sleep(0.1)
-            run_process.close()
+            
             print("run_process terminated")
+            
+            #if we close the window the spawned child processes terminate
+             
+            running_model.terminate()
+            while running_model.is_alive() == True:
+                time.sleep(0.1)
+            print("running_model terminated")    
             event.accept()
-            run_process.terminate() #if we close the window the spawned child process terminates
             print('Window closed')
         else:
             event.ignore()
 
-def spawned_process(q):
+def spawned_stream_process(band_q,model_q):
     '''
     initialize stream object and run stream.run method
     this will be the target of the stream_process
     '''
     stream = Stream()
-    stream.run(q)
+    stream.run(band_q,model_q)
+
+def spawned_model_process(model_q,art_q):
+
+    liveModel = LiveModel('model/cnn_time_dom.h5',model_q,art_q)
+    liveModel.run()
 
 def main():
     
@@ -110,22 +124,33 @@ def main():
     # showScreen(inputSize)
     sys.exit(qapp.exec_())
     
-    
 
 def launchArtScreen(size, artFeatures):    
-    #initialize multiprocessing queue to allow data transfer between child process
+    
     inputSize = size
+    #initialize multiprocessing queue to allow data transfer between runningstream and artScreen
+    band_q = Queue(5)
+    #initialize queue for runningstream and classifer
+    model_q = Queue(5)
+    #initiealize queue for classifier and artScreen
+    art_q = Queue(5)
     #initialize run_process for parallelism and make the variable global to allow 
     #exiting to make terminate the child process
+    
     global run_process
-    run_process = Process(target = spawned_process, args = (q,))
+    run_process = Process(target = spawned_process, args = (band_q,model_q))
     run_process.start() 
+
+    global running_model
+    running_model = Process(target = spawned_model_process, args=(model_q,art_q))
+    running_model.start()
+
 
     art = artScreen()
     art.setAttribute(Qt.WA_DeleteOnClose)
-    art.artDialog(inputSize,artFeatures, q)
+    art.artDialog(inputSize,artFeatures, band_q,model_q)
 
-q = Queue(5)
+
 
 
 if __name__=='__main__':
